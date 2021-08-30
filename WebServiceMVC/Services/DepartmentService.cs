6d2bcbd5c029b4749f091d44f027b161ed625c2d
Jonatas Santos Sá <jonatas.sa.25@gmail.com>
2021-08-30 12:58:11 -0300

    Atualizndo a Edit.cshtml

--- WebServiceMVC/Services/DepartmentService.cs
+++ WebServiceMVC/Services/DepartmentService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebServiceMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebServiceMVC.Services
 {
@@ -14,9 +15,9 @@ namespace WebServiceMVC.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(p => p.Name).ToList();
+            return await _context.Department.OrderBy(p => p.Name).ToListAsync();
         }
     }
 }
